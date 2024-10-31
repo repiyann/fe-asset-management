76269135306c31151d383fc021afd492d1a0aaf9
repiyann <muhrepiyan26@gmail.com>
2024-10-31 10:43:11 +0700

    feat(validator): add validator

---
+++ src/schema/masters.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const locationSchema = z.object({
+  name: z.string().min(8).max(50),
+  address: z.string().min(8).max(255),
+});
+
+export const masterDataSchema = z.object({
+  name: z.string().min(8).max(50),
+});
