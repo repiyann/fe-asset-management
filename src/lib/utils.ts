beff2121ed80c97c15752873186d088fce6e77a2
repiyann <muhrepiyan26@gmail.com>
2024-11-02 18:27:23 +0700

    feat(utils): new formatDate and currencyFormat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { format } from 'date-fns'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -9,4 +10,16 @@ export function generateSlug(title: string) {
   let slug = title.replace(/ - /g, '_')
   slug = slug.replace(/\s+/g, '-')
   return slug
+}
+
+export function formatDate(dateString: string) {
+  const date = new Date(dateString)
+  return format(date, 'd MMMM yyyy')
+}
+
+export function currencyFormat(value: number) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+  }).format(value)
 }
